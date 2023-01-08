68ab04caad90ae7016d1664a2e27120a65ea1c41
Andrey Zhvakin <barmaglot92@gmail.com>
2023-01-08 12:24:50 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,10 +40,8 @@ fi
 echo "==> Mounting S3 Filesystem ${AWS_S3_MOUNTPOINT}"
 # mkdir -p ${AWS_S3_MOUNTPOINT}
 
-echo $AWS_S3_BUCKET_NAME
-
 # s3fs mount command
-s3fs -d -o default_acl=public-read -o passwd_file=${AWS_S3_AUTHFILE} -o url=${AWS_S3_URL} -o allow_other ${AWS_S3_BUCKET_NAME} ${AWS_S3_MOUNTPOINT}
+s3fs -d -o passwd_file=${AWS_S3_AUTHFILE} -o url=${AWS_S3_URL} ${AWS_S3_BUCKET_NAME} ${AWS_S3_MOUNTPOINT}
 
 # RUN NGINX
 nginx
